943506462ae51d72948d6433a5ce95ad3d4e3024
Ajama0 <abasjama04@hotmail.com>
2024-11-22 22:26:02 +0000

    q1(Arrays and Hashing)

---
+++ Arrays and Hashing/q1.py
@@ -0,0 +1,30 @@
+from typing import List
+
+"""
+Given an integer array nums, 
+return true if any value appears more than once in the array, otherwise return false.
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+
+"""
+
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool: 
+        len_nums = len(nums)
+        set_nums = set(nums)
+        len_set = len(set_nums)
+
+        if(len_nums != len_set):
+            return True
+
+        return False    
+
+
+         
+solution = Solution()
+
+print(solution.hasDuplicate([1,2,3,3]))
